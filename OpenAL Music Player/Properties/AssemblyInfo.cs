7ba7196ce798c4ec7aec184650f0dc9f6d9c5e4a
LAGonauta <lagonauta@gmail.com>
2017-07-08 12:42:19 -0300

    Normalized and tweaked GUI margins
    Added a tooltip for the effects
    Changed version number preparing for release
    Changed assembly information

--- OpenAL Music Player/Properties/AssemblyInfo.cs
+++ OpenAL Music Player/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenAL Music Player")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple music player that focuses on playback using OpenAL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenAL Music Player")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.5.0")]
+[assembly: AssemblyFileVersion("0.9.5.0")]
