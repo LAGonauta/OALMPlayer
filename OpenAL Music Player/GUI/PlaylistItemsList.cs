5ee779c62753aa3a32f8e6c7589f1ed1dd81d9d4
LAGonauta <lagonauta@gmail.com>
2022-08-03 18:41:01 -0300

    Added missing fields

--- OpenAL Music Player/GUI/PlaylistItemsList.cs
+++ OpenAL Music Player/GUI/PlaylistItemsList.cs
@@ -6,6 +6,8 @@
     public string Title { get; set; }
     public string Performer { get; set; }
     public string Album { get; set; }
+    public uint DiscNumber { get; set; }
     public string FileName { get; set; }
+    public uint TrackNumber { get; set; }
   }
 }
