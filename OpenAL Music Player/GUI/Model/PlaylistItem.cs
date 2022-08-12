02cafe2d8eb153cbda78bb09f3a21d83ea28dbda
LAGonauta <lagonauta@gmail.com>
2022-08-12 06:07:26 -0300

    Use nameof

--- OpenAL Music Player/GUI/Model/PlaylistItem.cs
+++ OpenAL Music Player/GUI/Model/PlaylistItem.cs
@@ -20,13 +20,13 @@ namespace OpenALMusicPlayer.GUI.Model
       PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    public string FilePath { get => filePath; set { filePath = value; OnPropertyChanged("FilePath"); } }
-    public string Number { get => number; set { number = value; OnPropertyChanged("Number"); } }
-    public string Title { get => title; set { title = value; OnPropertyChanged("Title"); } }
-    public string Performer { get => performer; set { performer = value; OnPropertyChanged("Performer"); } }
-    public string Album { get => album; set { album = value; OnPropertyChanged("Album"); } }
-    public uint DiscNumber { get => discNumber; set { discNumber = value; OnPropertyChanged("DiscNumber"); } }
-    public string FileName { get => fileName; set { fileName = value; OnPropertyChanged("FileName"); } }
-    public uint TrackNumber { get => trackNumber; set { trackNumber = value; OnPropertyChanged("TrackNumber"); } }
+    public string FilePath { get => filePath; set { filePath = value; OnPropertyChanged(nameof(FilePath)); } }
+    public string Number { get => number; set { number = value; OnPropertyChanged(nameof(Number)); } }
+    public string Title { get => title; set { title = value; OnPropertyChanged(nameof(Title)); } }
+    public string Performer { get => performer; set { performer = value; OnPropertyChanged(nameof(Performer)); } }
+    public string Album { get => album; set { album = value; OnPropertyChanged(nameof(Album)); } }
+    public uint DiscNumber { get => discNumber; set { discNumber = value; OnPropertyChanged(nameof(DiscNumber)); } }
+    public string FileName { get => fileName; set { fileName = value; OnPropertyChanged(nameof(FileName)); } }
+    public uint TrackNumber { get => trackNumber; set { trackNumber = value; OnPropertyChanged(nameof(TrackNumber)); } }
   }
 }
